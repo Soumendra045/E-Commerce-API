0922d8c79234af475fe4e1e3aa74c20527e0ae8d
Soumendra Rout <soumendra096@gmail.com>
2025-09-16 10:53:05 +0530

    Updated Mornibg code 16

--- E_Commerce_API/urls.py
+++ E_Commerce_API/urls.py
@@ -21,6 +21,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/',include('users.urls')),
@@ -33,6 +39,12 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Jwt Authentication:
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -4,16 +4,19 @@ from .models import User,UserProfile
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import AllowAny
 
 User = get_user_model()
 
 @extend_schema(
     tags=['User-Registration'],
-    description='User registration here'
+    description='User registration here',
+    auth=[] 
 )
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 @extend_schema(
     tags=['User'],
