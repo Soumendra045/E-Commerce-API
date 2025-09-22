c954b36d8895fbd432f9cedf920a525f87bf492e
Soumendra Rout <soumendra096@gmail.com>
2025-09-22 23:28:43 +0530

    Updated now 22

--- E_Commerce_API/settings.py
+++ E_Commerce_API/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "static/media" 
+MEDIA_ROOT = BASE_DIR / "static/" 
 # MEDIA_ROOT = os.path.join(BASE_DIR,'static/media')
 
 # Default primary key field type
